c8c025fc57cf6695f87af1800ec3e77f13ea545c
Keith Johns <kjohns@msprecovery.com>
2022-11-01 11:42:14 -0400

    testing gh-pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { ApolloProvider } from "@apollo/client";
 import client from "./ApolloClient";
@@ -9,9 +9,9 @@ import client from "./ApolloClient";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
-    <BrowserRouter>
+    <HashRouter>
       <App id="app" />
-    </BrowserRouter>
+    </HashRouter>
   </ApolloProvider>
 );
 
